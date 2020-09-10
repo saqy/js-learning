0808da60f2bddad522f90ca233b2dcd4f2c976ea
usman <uali7930@gmail.com>
2020-09-10 22:39:58 +0500

    latest changes

--- concat.js
+++ concat.js
@@ -5,3 +5,12 @@ let array2=["usman","ali"];
 console.log("Orgional Array :  " + array);
 let newarr= array.concat(array2);
 console.log(" After concat , array.concat(0,1) :     " , newarr );
+
+
+//concat values to array(nested) 
+
+let name=["usman",23,"male"];
+
+const new_names=name.concat(1,2,[2,3]);
+
+console.log("concat values to array " ,new_names);
