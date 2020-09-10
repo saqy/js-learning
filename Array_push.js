2991929cbad8524933dd55e984dbe27c3752857e
usman <uali7930@gmail.com>
2020-09-10 22:26:22 +0500

    latest

--- Array_push.js
+++ Array_push.js
@@ -1,6 +1,14 @@
+//Simple Push Function implementation
 let arr=[1,2,3,4,5,6,7];
+arr.push("Usman","ali");
 
-console.log(arr);
+//copy an array with the help of push function.
+let copy=[];
+arr.forEach(function(item){
+    copy.push(item);
+})
+
+console.log(" First Array : ", arr ," Copy of Array : ", copy);
 
 
 
--- helloWorld.js
+++
@@ -1,1 +0,0 @@
-console.log("hello usman");console.log("hello usman");console.log("hello usman");console.log("hello usman");console.log("hello usman");
