40356a01e9561517c736c946d32bf0a3c27827e5
usman <uali7930@gmail.com>
2020-09-10 22:35:13 +0500

    latest changes

--- concat.js
+++ concat.js
@@ -1,8 +1,7 @@
+//concat two arrays with each other
 let array=["usman","ali","khan","kashif"];
 let array2=["usman","ali"];
 
 console.log("Orgional Array :  " + array);
-
 let newarr= array.concat(array2);
 console.log(" After concat , array.concat(0,1) :     " , newarr );
-
