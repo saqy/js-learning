50cafe66e23a039b68a53387b851e11ec33455e8
Kaleem <kaleem.iplex@gmail.com>
2020-09-12 01:48:17 +0500

    - zero change

--- practice2.js
+++ practice2.js
@@ -1,6 +1,6 @@
 //Qno4
 console.log("=============Question No 4=============");
-let count = 6;
+let count = 5;
 let output = "";
 for (let i = 1; i <= count; i++) {
   output = " ";
@@ -18,11 +18,14 @@ console.log("=============Question No 5=============");
 let inputArray = [0, -1, 2];
 let max = 0;
 let pairs = [];
+let product = 0;
 for (let i = 0; i < inputArray.length; i++) {
   if (inputArray[i + 1] !== undefined) {
-    let product = inputArray[i] * inputArray[i + 1];
-    if (product >= max) {
-      max = product;
+    product = inputArray[i] * inputArray[i + 1];
+    if (max <= product) {
+      if (product > 0) {
+        max = product;
+      }
       pairs = [inputArray[i], inputArray[i + 1]];
     }
   }
