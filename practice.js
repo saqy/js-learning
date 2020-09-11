45ffde792bd5572e30ca28df665447756b2cb850
Hussein <mamunanaqvi96@gmail.com>
2020-09-11 23:58:59 +0500

    adds todays task

--- practice.js
+++ practice.js
@@ -1,3 +1,44 @@
+// Create a nested Create a nested for loops produce the loops produce the
+// following output.
+
+// ....1
+// ...22
+// ..333
+// .4444
+// 55555
+
+
+let count = 5;
+let output = "";
+for (let p = 1; p <= count; p++) {
+  output = "";
+  for (let q = 1; q <= count - p; q++) {
+  output += ".";
+  }
+  for (let s = 1; s <= p; s++) {
+      output += p;
+  }
+  console.log(output);
+}
+
+// Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+// Example
+// For inputArray = [3, 6, -2, -5, 7, 3], the output should be adjacentElementsProduct(inputArray) = 21.
+// 7 and 3 produce the largest product.
+
+
+let inputArray= [3, 6, -2, -5, 7, 3];
+let result= inputArray[0]*inputArray[1];
+for(let i=1; i<inputArray.length; i++){
+    let x= inputArray[i] * inputArray[i+1];
+    if(x > result){
+        result = x;
+    };
+};
+console.log(result);
+
+
+///////////////////SEPTEMBER 10 TASK////////////////////////
 // Write a program to reverse an array or string using loop
 // input: [ 1, 2, 3 ]
 // output: [ 3, 2, 1 ]
@@ -33,4 +74,6 @@ for(let k=1; k<=3; k++){
     input.unshift(input.pop());
     console.log(input);
 }
+
+
 
