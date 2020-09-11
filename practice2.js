0fce13fe8a2941f843b391fac66782f566915f87
Kaleem <kaleem.iplex@gmail.com>
2020-09-12 01:10:07 +0500

    bugs in practice

--- practice2.js
+++ practice2.js
@@ -1,6 +1,6 @@
 //Qno4
 console.log("=============Question No 4=============");
-let count = 5;
+let count = 6;
 let output = "";
 for (let i = 1; i <= count; i++) {
   output = " ";
@@ -15,16 +15,17 @@ for (let i = 1; i <= count; i++) {
 
 //Qno5
 console.log("=============Question No 5=============");
-let inputArray = [-500, 100, 2, 8, -1000, -300, 4];
+let inputArray = [0, -1, 2];
 let max = 0;
 let pairs = [];
 for (let i = 0; i < inputArray.length; i++) {
-  if (inputArray[i + 1] !== undefined && inputArray[i + 2] !== undefined) {
+  if (inputArray[i + 1] !== undefined) {
     let product = inputArray[i] * inputArray[i + 1];
-    if (product > max) {
+    if (product >= max) {
       max = product;
       pairs = [inputArray[i], inputArray[i + 1]];
     }
   }
 }
 console.log(pairs);
+console.log(max);
