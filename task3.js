f58e3a4e5f2f1a3e116f1403cf0dfff2a352ea5f
usman <uali7930@gmail.com>
2020-09-15 19:38:18 +0500

    Array Functions and normal functions -14-09-2020

--- task3.js
+++ task3.js
@@ -31,14 +31,9 @@ console.log(" Centuary : ",centuryFromYear(1999));
 console.log("//Question 9");
 var fruits = ['banana', 'cherry', 'cactus','orange', 'apple','cherry', 'orange', 'apple', 'banana', 'cherry', 'orange', 'fig' ];
 var count = fruits.reduce(function(counts, number) {
-   if (counts[number] === undefined) {
-     counts[number] = 1;
-   } else {
-     counts[number]+=1;
-   }
-   return counts;
-    console.log(number);
- }, []);
+    counts[number] === undefined?counts[number] = 1: counts[number]+=1; return counts; 
+}, 
+[]);
 console.log(count)
 //-------------------------------------//
 
