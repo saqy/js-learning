e98a76438952d3238ec7dec56e43666d880c1fc1
usman <uali7930@gmail.com>
2020-09-18 20:59:52 +0500

    My queue Task on 18-09-20202

---
+++ queue_task.js
@@ -0,0 +1,42 @@
+let queue_array=[];
+let stack_array=[];
+
+//Stack Implememntation
+function addtostack(val){
+    return stack_array.push(val);     
+}
+function removestack(){
+    return stack_array.pop();  
+}
+
+//Queue Implememntation
+function addtoqueue(val){
+    return queue_array.push(val);     
+}
+let ar=[];
+
+function removequeue(){
+    for(let i=1;i<=queue_array.length-1;i++){      
+        ar.push(queue_array[i]);           // queue_array.pop()        
+    }
+    queue_array=ar;
+}
+
+addtostack(1);
+addtostack(2);
+addtostack(3);
+console.log("Origional Stack Array : ",stack_array)
+removestack();
+console.log("After Remove From Stack : ",stack_array);
+
+addtoqueue(1)
+addtoqueue(2)
+addtoqueue(3)
+addtoqueue(4)
+addtoqueue(5)
+addtoqueue(6)
+console.log("Origional Queue Array : ",queue_array)
+removequeue();
+
+
+console.log("After Remove From Queue : ",queue_array)
