518c3025d0f2c4a4e733209ee89eadca6840c34e
ishaq001 <orakk2211@gmail.com>
2020-09-21 18:03:26 +0500

    example five added in reduce practice

--- reduce.js
+++ reduce.js
@@ -72,3 +72,39 @@ return acc + curr.price
  
  console.log(flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
 
+ //Example 5 
+
+ var voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+];
+
+function voterResults(arr) {
+ return arr.reduce((acc,curr)=>{
+if((curr.age>= 18 && curr.age<= 25)&&(curr.voted === true)){
+    return `NumYoungVotes : ${acc + curr}`
+}
+let a = `NumYoungPeople : ${acc + curr.voted}`
+ },{})
+}
+
+console.log(voterResults(voters)); // Returned value shown below:
+/*
+{ numYoungVotes: 1,
+  numYoungPeople: 4,
+  numMidVotesPeople: 3,
+  numMidsPeople: 4,
+  numOldVotesPeople: 3,
+  numOldsPeople: 4 
+}
+*/
