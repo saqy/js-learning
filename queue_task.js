b46cdea08094ec0616f17ffaa238556b71fc1525
usman <uali7930@gmail.com>
2020-09-21 21:31:21 +0500

    My Queue Task on 21-09-2020

--- queue_task.js
+++ queue_task.js
@@ -2,6 +2,7 @@ let queue_array=[];
 let stack_array=[];
 
 //Stack Implememntation
+
 function addtostack(val){
     return stack_array.push(val);     
 }
@@ -15,29 +16,45 @@ function addtoqueue(val){
 }
 
 
+// function removequeue(){
+//     let temp=[];
+//     let len =queue_array.length;
+//     for(let i=0;i<len;i++){   
+//         if(len==0){
+//             temp.push(queue_array.pop());           // queue_array.pop()        
+//             console.log("tem",temp);
+//             queue_array=temp;
+//         }   
+//         // queue_array=temp;
+//     }
+//     // queue_array=temp;
+// }
+
 function removequeue(){
     let temp=[];
-    for(let i=1;i<=queue_array.length-1;i++){      
-        temp.push(queue_array[i]);           // queue_array.pop()        
+    let queue_len =queue_array.length;
+    for(let i=0;i<=queue_len;i++){   
+        i==queue_len?  temp.pop(): temp.push(queue_array.pop());      
     }
-    queue_array=temp;
+    let temp_length=temp.length;
+    for(let i=0;i<temp_length;i++){  
+        queue_array.push(temp.pop());              
+    }
 }
 
 addtostack(1);
 addtostack(2);
 addtostack(3);
-console.log("Origional Stack Array : ",stack_array)
+console.log("Original Stack Array    ",stack_array)
 removestack();
-console.log("After Remove From Stack : ",stack_array);
+console.log("After Remove From Stack ",stack_array);
 
 addtoqueue(1);
 addtoqueue(2);
 addtoqueue(3);
-addtoqueue(4);
-addtoqueue(5);
-addtoqueue(6);
-console.log("Origional Queue Array : ",queue_array);
+console.log("Original Queue Array :  ",queue_array);
 removequeue();
 
 
-console.log("After Remove From Queue : ",queue_array);
+
+console.log("After Remove From Queue ",queue_array);
