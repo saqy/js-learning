187299fbdc2c5d202d5b01c92c4ba3c89581f453
ishaq001 <orakk2211@gmail.com>
2020-09-22 18:09:20 +0500

    queue task

--- queuetask.js
+++ queuetask.js
@@ -1,15 +1,15 @@
-let queueArray = []
-let stackArray = []
+// let queueArray = []
+// let stackArray = []
 
-function addToQueue(value){
-let addToArray = queueArray.push(value)
-return addToArray;
-}
+// function addToQueue(value){
+// let addToArray = queueArray.push(value)
+// return addToArray;
+// }
 
-console.log('Adding 1 to Queue',addToQueue(1))
-console.log('Adding 2 to Queue',addToQueue(2))
-console.log('Adding 3 to Queue',addToQueue(3))    
-console.log('Queue Array',queueArray)
+// console.log('Adding 1 to Queue',addToQueue(1))
+// console.log('Adding 2 to Queue',addToQueue(2))
+// console.log('Adding 3 to Queue',addToQueue(3))    
+// console.log('Queue Array',queueArray)
 
 
 // function removeFromQueue(){
@@ -24,25 +24,48 @@ console.log('Queue Array',queueArray)
 
 
 
-function addToStack(value){
-    let addToArray = stackArray.push(value)
-    return addToArray;
+// function addToStack(value){
+//     let addToArray = stackArray.push(value)
+//     return addToArray;
     
+// }
+
+// console.log('Adding 1 to stack',addToStack(1))
+// console.log('Adding 2 to stack',addToStack(2))
+// console.log('Adding 3 to stack',addToStack(3))    
+// console.log(' stack array',stackArray)
+
+// function removeFromStack(){
+//     let RemoveFromArray = stackArray.pop()
+//     return RemoveFromArray;
+    
+// }
+
+// console.log(' Removing last from stack',removeFromStack())
+// console.log(' Removing last from stack',removeFromStack())
+// console.log(' Removing last from stack',removeFromStack())    
+// console.log('stack Array', stackArray)
+
+
+
+
+let queue = [1,2,3,4];
+
+function removeFromQueue(){
+  let Length = queue.length;
+  let temp = [];
+  for (let i = 0; i < Length; i++) {
+    temp.push(queue.pop());
+  }
+  
+ queue = temp;
+  temp.pop();
+return temp  
+
 }
 
-console.log('Adding 1 to stack',addToStack(1))
-console.log('Adding 2 to stack',addToStack(2))
-console.log('Adding 3 to stack',addToStack(3))    
-console.log(' stack array',stackArray)
+console.log(removeFromQueue())
+console.log(removeFromQueue())
+console.log(removeFromQueue())
 
-function removeFromStack(){
-    let RemoveFromArray = stackArray.pop()
-    return RemoveFromArray;
-    
-}
-
-console.log(' Removing last from stack',removeFromStack())
-console.log(' Removing last from stack',removeFromStack())
-console.log(' Removing last from stack',removeFromStack())    
-console.log('stack Array', stackArray)
 
