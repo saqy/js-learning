882e8e629d1ffbe839a8b31ab8ea19c848588eff
Kaleem <kaleem.iplex@gmail.com>
2020-09-22 18:46:14 +0500

    final code

--- practice6.js
+++ practice6.js
@@ -1,17 +1,18 @@
 //Qno1
 let queue = [];
 let stack = [];
+let withOutRear = [];
 function addToQueue(value) {
   queue.push(value);
 }
 function removeFromQueue() {
   let queueLength = queue.length;
-  let withOutRear = [];
   for (let i = 0; i < queueLength; i++) {
     withOutRear.push(queue.pop());
   }
-  withOutRear.pop();
+  console.log(withOutRear);
   queue = withOutRear;
+  queue.pop();
 }
 
 function addToStack(value) {
@@ -23,23 +24,8 @@ function removeFromStack() {
 
 addToQueue(1);
 addToQueue(3);
-console.log("Que after add", queue);
-removeFromQueue();
-console.log("Que after removal", queue);
-addToQueue(10);
-console.log("Que after add", queue);
-removeFromQueue();
-console.log("Que after removal", queue);
-addToQueue(15);
+addToQueue(4);
 console.log("Que after add", queue);
 removeFromQueue();
 removeFromQueue();
-console.log("Que after removal", queue);
-
-// function factorial(n) {
-//   if (n === 1) {
-//     return 1;
-//   }
-
-//   return n * factorial(n - 1);
-// }
+console.log("Que after remove", queue);
