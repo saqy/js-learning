558b316307225c1080c6da639eb8c86634f59bcb
Kaleem <kaleem.iplex@gmail.com>
2020-09-24 20:29:29 +0500

    callback exampleS

--- FunctionsExample/CallBack.js
+++ FunctionsExample/CallBack.js
@@ -12,3 +12,77 @@ function processUserInput(callback, func) {
 }
 
 processUserInput(greeting, testUser);
+
+console.log("===============example2================");
+// callbacks
+
+function download(cb) {
+  setTimeout(() => {
+    console.log("donloading..");
+
+    cb();
+  }, 3000);
+}
+
+function process(cb) {
+  setTimeout(() => {
+    console.log("prosessing..");
+
+    cb();
+  }, 1000);
+}
+
+function execute(cb) {
+  setTimeout(() => {
+    console.log("execute..");
+
+    cb();
+  }, 3000);
+}
+
+function display() {
+  console.log("display..");
+}
+
+// download(function () {
+//   process(function () {
+//     execute(display);
+//   });
+// });
+// download(function () {
+//   process(function () {
+//     execute(function () {
+//       display();
+//     });
+//   });
+// });
+console.log("===============example3================");
+function printString(string) {
+  setTimeout(() => {
+    console.log(string);
+  }, 10000);
+}
+// function printAll() {
+//   printString("A");
+//   printString("B");
+//   printString("C");
+// }
+function printAll() {
+  printString("Arrerre", () => {
+    printString("B", () => {
+      printString("C", () => {});
+    });
+  });
+}
+printAll();
+// printString("Test");
+
+console.log("===============example4=================");
+
+console.log("===============example5=================");
+function printString(string, callback) {
+  setTimeout(() => {
+    console.log(string);
+    callback();
+  }, Math.floor(Math.random() * 100) + 1);
+}
