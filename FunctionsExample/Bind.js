6f6cfec00f1eddc1f25a2a2e353904429b81283a
Kaleem <kaleem.iplex@gmail.com>
2020-09-24 18:24:17 +0500

    more examples

--- FunctionsExample/Bind.js
+++ FunctionsExample/Bind.js
@@ -17,3 +17,20 @@ var pokemonName = function (secondName) {
 var logPokemon = pokemonName.bind(pokemon); // creates new object and binds pokemon. 'this' of pokemon === pokemon now
 
 logPokemon("TestName");
+console.log("=======================example12==============");
+let runner = {
+  name: "Runner",
+  run: function (speed) {
+    console.log(this.name + " runs at " + speed + " mph.");
+  },
+};
+
+let flyer = {
+  name: "Flyer",
+  fly: function (speed) {
+    console.log(this.name + " flies at " + speed + " mph.");
+  },
+};
+let run = runner.run.bind(flyer, 20);
+console.log("run", run);
+console.log(run());
