400ea8fb0427f98dcc1789909151d81332a0e57f
Hussein <mamunanaqvi96@gmail.com>
2020-09-25 04:53:00 +0500

    pushes FIFO LIFO task

--- practice2.js
+++ practice2.js
@@ -11,41 +11,40 @@
 // addtoQueue(value); //add to the begining
 // removeQueue();  // return first inserted value
 
-let stack= []; //lifo
-let queue= []; //fifo
+        let stack= []; //lifo
+        let queue= []; //fifo
 
-let temp=[];
+        let temp=[];
 
-function addtoStack(value) {
-  stack.push(value);
-  console.log(stack);
-}
+        function addtoStack(value) {
+          stack.push(value);
+          console.log(stack);
+        }
 
-function removeFromStack() { 
-  stack.pop();
-  console.log(stack);
-}
-function addtoQueue(value) {
-  queue.push(value);
-}
+        function removeFromStack() { 
+          stack.pop();
+          console.log(stack);
+        }
+        function addtoQueue(value) {
+          queue.push(value);
+        }
 
-function removeFromQueue() { 
-  let queueLength = queue.length;
-  for (let i=0; i<queueLength; i++ ) {
-    temp.push(queue.pop())
-  }
-  queue=temp;
-  console.log('\nLIFO '+queue.pop());
-}
+        function removeFromQueue() { 
+          let queueLength = queue.length;
+          for (let i=0; i<queueLength; i++ ) {
+            temp.push(queue.pop())
+          }
+          queue=temp;
+          console.log('\nLIFO '+queue.pop());
+        }
 
-
-addtoQueue(10);
-addtoQueue(20);
-addtoQueue(30);
-addtoQueue(40);
-removeFromQueue();
-removeFromQueue();
-removeFromQueue();
+        addtoQueue(10);
+        addtoQueue(20);
+        addtoQueue(30);
+        addtoQueue(40);
+        removeFromQueue();
+        removeFromQueue();
+        removeFromQueue();
 
 
 
