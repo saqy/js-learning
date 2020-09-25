94207541187332a1a6ed5ee322fc1aba5ec7012d
ishaq001 <orakk2211@gmail.com>
2020-09-25 19:59:12 +0500

    async await

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,20 +1,44 @@
-function delayedUpperCase(input,n){
-    return new Promise((resolve,reject)=>{
-    setTimeout( () => {
-        return typeof input === 'string' ? resolve(input.toUpperCase()) : reject(input)
-              },n)
-    }).then(data=>{console.log(data)})
-    .catch(e=>console.log(e))
+async function delayedUpperCase(input,n){
+    try {
+        const data = await new Promise((resolve, reject) => {
+            setTimeout(() => {
+                return typeof input === 'string' ? resolve(input.toUpperCase()) : reject(input);
+            }, n);
+        });
+        console.log(data);
+    } catch (e) {
+        return console.log(e);
+    }
 }
 
 
-//delayedUpperCase('ishaq',1000)
+delayedUpperCase('ishaq',1000)
 
 
 
-async function output() {
-    const output = await delayedUpperCase('ishaq',1000);
-   return output
-  }
+// async function output() {
+//     const output = await delayedUpperCase('ishaq',1000);
+//    return output
+//   }
 
-  output()
+//   output()
+
+
+
+
+//   function delayedUpperCase(input,n){
+//     return new Promise((resolve,reject)=>{
+//     setTimeout( () => {
+//         return typeof input === 'string' ? resolve(input.toUpperCase()) : reject(input)
+//               },n)
+//     }).then(data=>{console.log(data)})
+//     .catch(e=>console.log(e))
+// }
+
+
+// async function output() {
+//     const output = await delayedUpperCase('ishaq',1000);
+//    return output
+//   }
+
+//   output()
