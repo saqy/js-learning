3292c916a14b8cea5983b45bff1a8118aeb3f3d5
uali7930 <uali7930@gmail.com>
2020-09-25 07:17:19 -0700

    Promises/async Await -TAsk

--- Qno15.js
+++ Qno15.js
@@ -4,7 +4,11 @@ console.log("= = = Q no 15 = = = Using Async Await = = = =")
 delayedUpperCase2 = async (input, n)=>{
     let pro = new Promise( (resolve,reject)=>{
         setTimeout(()=>{
-            return typeof input === "string" ? resolve(console.log(input.toUpperCase())): reject(input)
+            return typeof input === "string"
+            ? 
+            resolve(console.log(input.toUpperCase()))
+            :
+             reject(input)
         },n)
     })
     let res= await pro;
