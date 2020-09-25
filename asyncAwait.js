ad38202bb0b5eff57954a7f8c9f00d6de81e79a4
ishaq001 <orakk2211@gmail.com>
2020-09-25 18:45:22 +0500

    async await task

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,10 +1,20 @@
-async function delayedUpperCase(input,n){
-    let promise =  new Promise((resolve,reject)=>{
-        setTimeout(() => {
-             return typeof input === 'string' ? resolve(input.toUpperCase()) : reject(input)
-            }, n)
-    })
-    let result = await promise
-console.log(result)
+function delayedUpperCase(input,n){
+    return new Promise((resolve,reject)=>{
+    setTimeout( () => {
+        return typeof input === 'string' ? resolve(input.toUpperCase()) : reject(input)
+              },n)
+    }).then(data=>{console.log(data)})
+    .catch(e=>console.log(e))
 }
-delayedUpperCase('ishaq',1000)
+
+
+//delayedUpperCase('ishaq',1000)
+
+
+
+async function output() {
+    const output = await delayedUpperCase('ishaq',1000);
+   return output
+  }
+
+  output()
