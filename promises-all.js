c0db2bac8f9d68f064021684f0d799457a76ae80
ishaq001 <orakk2211@gmail.com>
2020-09-25 19:32:32 +0500

    promises all

---
+++ promises-all.js
@@ -0,0 +1,10 @@
+let promise1 = Promise.resolve('resolved')
+let promise2 = Promise.resolve(2)
+//let promise3 = Promise.reject('rejected')
+let promise4 = new Promise((res,rej)=>{
+    setTimeout(()=>{
+        res('inside setTimeOut')
+    },1000)
+})
+
+Promise.all([promise1,promise2,promise4]).then(val=>console.log(val)).catch(e=>console.log(e))
