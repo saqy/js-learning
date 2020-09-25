d352b816638474b503ca66aba7c38ce689a88b73
Kaleem <kaleem.iplex@gmail.com>
2020-09-25 21:45:41 +0500

    practice.js

--- practice.js
+++ practice.js
@@ -1,22 +1,22 @@
 //Qno14
 // console.log("===================Qno14=======================");
-function delayedUpperCase(input, n) {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      if (typeof input === "string") {
-        res(input.toUpperCase());
-      } else {
-        rej(input);
-      }
-    }, n);
-  });
-}
+// function delayedUpperCase(input, n) {
+//   return new Promise((res, rej) => {
+//     setTimeout(() => {
+//       if (typeof input === "string") {
+//         res(input.toUpperCase());
+//       } else {
+//         rej(input);
+//       }
+//     }, n);
+//   });
+// }
 
-delayedUpperCase("test", 1000)
-  .then((result) => console.log("Success:", result))
-  .catch((error) => {
-    console.log("Error:", error);
-  });
+// delayedUpperCase("test", 1000)
+//   .then((result) => console.log("Success:", result))
+//   .catch((error) => {
+//     console.log("Error:", error);
+//   });
 // console.log("===================Qno15=======================");
 //Qno15
 // async function withAsyncAwait() {
@@ -28,17 +28,18 @@ delayedUpperCase("test", 1000)
 //   }
 // }
 // withAsyncAwait();
-
-async function delayedUpperCase1(input, n) {
-  //   return new Promise((res, rej) => {
-  let result = await setTimeout(() => {
+function delayedUpperCase1(input, n) {
+  setTimeout(() => {
     if (typeof input === "string") {
-      return input.toUpperCase();
+      return "test";
     } else {
-      return input;
+      throw new Error(`HTTP error! status:`);
     }
   }, n);
-  return result;
-  //   });
 }
-console.log("simple async await", delayedUpperCase1("test2", 1000));
+async function result(input, n) {
+  let result = await delayedUpperCase1(input, n);
+  // let result2 = await result;
+  return result;
+}
+console.log("simple async await", result("Test2", 1000));
