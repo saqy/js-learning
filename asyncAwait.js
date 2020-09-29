d85e7225788af850d535b95d04d026866dc20f3b
Hussein <mamunanaqvi96@gmail.com>
2020-09-29 18:33:41 +0500

    pushes async await changes by ss

--- asyncAwait.js
+++ asyncAwait.js
@@ -1,60 +1,114 @@
     //    async await
 
-            async function download(){
-                return new Promise((resolve, reject)=>{
-                    setTimeout(()=> {
-                        console.log('downloading..');
+            // async function download(){
+            //     return new Promise((resolve, reject)=>{
+            //         setTimeout(()=> {
+            //             console.log('downloading..');
+            //             const err= true;
+            //             if(err){
+            //                 resolve();
+            //             } else {
+            //                 reject('error in downloading');
+            //             }
+                        
+            //         }, 3000)
+            //     })
+                
+            // }
+
+            // function process(){
+            //     return new Promise((resolve, reject)=>{
+            //         setTimeout(()=>{
+            //             console.log('processing..');
+            //             const err= true;
+            //             if(err){
+            //                 resolve();
+            //             } else {
+            //                 reject('error in processing');
+            //             }
+
+            //         },1000);
+            //     })
+            // }
+
+            // function execute(){
+            //     console.log('execute..');
+            // }
+
+            // async function trythis() {
+            //     try{
+            //         await download()
+            //         await process()
+            //         await execute()
+            //     }
+            //     catch(e){
+            //         console.log(err);
+            //     }
+            // }
+            // trythis();
+
+
+
+            //pending work:
+            //replace promises completely with async await
+            //To actually consume the value returned when the promise fulfills, 
+            //since it is returning a promise, 
+            //we could use a .then() block:
+            
+
+            // download.then((resolve,reject) => {
+            //     setTimeout(()=>{
+            //         console.log('downloading...');
+            //         const err=true;
+            //         if(err){
+            //             resolve();
+            //         } else {
+            //             reject('error in downloading');
+            //         }
+            //     },1000)
+            // })
+
+            function download(){
+                function then(resolve, reject){
+                    setTimeout(() => {
+
                         const err= true;
                         if(err){
-                            resolve();
+                            resolve('downloading..');
                         } else {
                             reject('error in downloading');
-                        }
-                        
-                    }, 3000)
-                })
-                
+                        }                       
+                    }, 1000)
+                }              
             }
 
-            function process(){
-                return new Promise((resolve, reject)=>{
-                    setTimeout(()=>{
-                        console.log('processing..');
+            const process = () => ({
+                then(resolve, reject){
+                    setTimeout(() => {
                         const err= true;
                         if(err){
-                            resolve();
+                            resolve('processing..');
                         } else {
                             reject('error in processing');
                         }
-
                     },1000);
-                })
-            }
+                }
+            })
 
-            function execute(){
+            function execute() {
                 console.log('execute..');
             }
 
             async function trythis() {
-                try{
-                    await download()
-                    await process()
-                    await execute()
+                try {
+                    console.log(await download())
+                    console.log(await process())
+                    console.log(await execute())
                 }
-                catch(e){
-                    console.log(err);
+                catch(e) {
+                    console.log(e);
                 }
             }
             trythis();
-
-
-            // download(function(){
-            //     process(execute)
-            // })
-
-            //pending work:
-            //replace promises completely with async await
-
-
-
+     
             
