d7d7c548496d18ce92de7c7a7775dab459aca5cb
uali7930 <uali7930@gmail.com>
2020-09-29 12:36:09 -0700

    Updated Q no 16

--- task6.js
+++ task6.js
@@ -1,19 +1,25 @@
 
-
-inreasingseq2=seq=> {
-    let issue = 0;    
-       for(let i = 0; i<seq.length;i++) {
-          if(seq[i+1] !== undefined && seq[i+2] !== undefined){
-            (seq[i] < seq[i+1] && seq[i+1] < seq[i+2])?  issue:   issue ++
-          }
-          if(issue > 1){
-              return false
-          }
+// Q no 16
+checkseq = seq =>{
+    for(let i = 0; i < seq.length; i++){
+        let duplicate = seq;
+        duplicate.splice(i,1);
+        if( isseq(duplicate) ){
+            return true;
         }
-        return true;
+    }
+    return false;
 }
 
+isseq = seq =>{
+  for(let j = 0; j < seq.length; j++){
+      if(seq[j] >= seq[j+1]){
+         return false;
+       }
+   }
+  return true;
+}
 
-seq=[5,2,3,4,5]
-let res2=inreasingseq2(seq)
-console.log( res2 )
+seq = [1,2,1,2]
+let res = checkseq(seq)
+console.log(res)
