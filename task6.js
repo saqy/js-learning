298ac7944b917c7767ebcc73c21d9c0ccf96ba54
uali7930 <uali7930@gmail.com>
2020-09-29 08:21:44 -0700

    29-09-2020 task Q no 16

--- task6.js
+++ task6.js
@@ -1,19 +1,19 @@
 
-console.log(' = = = Q no 16 = = = ')
-function inreasingseq(seq) {
-    let issue = 0;
-    for (let i=0;i<seq.length;i++) {
-        if( seq[i] >= seq[i+1] ) {
-            issue++;  
-            console.log('At index ',i,' Array Element  ',seq[i] ,' (',issue , 'Issue Occured )')   
-            if(issue > 1) 
-                return 'false'; 
-        }      
-    } 
-    return true;
+
+inreasingseq2=seq=> {
+    let issue = 0;    
+       for(let i = 0; i<seq.length;i++) {
+          if(seq[i+1] !== undefined && seq[i+2] !== undefined){
+            (seq[i] < seq[i+1] && seq[i+1] < seq[i+2])?  issue:   issue ++
+          }
+          if(issue > 1){
+              return false
+          }
+        }
+        return true;
 }
 
-let seq = [1,3,2,1];
-console.log('Array ',seq)
-let res = inreasingseq(seq)
-console.log( res )
+
+seq=[5,2,3,4,5]
+let res2=inreasingseq2(seq)
+console.log( res2 )
