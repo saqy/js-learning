914544067aac577ffa94b15dd7c9e9dcbc7f1709
Kaleem <kaleem.iplex@gmail.com>
2020-09-29 19:06:43 +0500

    more examples

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log("======================example1===================");
 const myFavouriteAuthors = {
   allAuthors: {
     fiction: ["Agatha Christie", "J. K. Rowling", "Dr. Seuss"],
@@ -61,3 +62,61 @@ for (const author of myFavouriteAuthors) {
 }
 
 console.log(...myFavouriteAuthors);
+console.log("======================example2===================");
+
+function* generatorFunction() {
+  // Line 1
+  console.log("This will be executed first.");
+  yield "Hello, "; // Line 2
+  console.log("I will be printed after the pause");
+  yield "World!";
+}
+const generatorObject = generatorFunction();
+console.log(generatorObject.next().value);
+console.log(generatorObject.next().value);
+console.log(generatorObject.next().value);
+console.log("===================example3=====================");
+const iterableObj = {
+  [Symbol.iterator]() {
+    let step = 0;
+    return {
+      next() {
+        step++;
+        if (step === 1) {
+          return { value: "This", done: false };
+        } else if (step === 2) {
+          return { value: "is", done: false };
+        } else if (step === 3) {
+          return { value: "iterable.", done: false };
+        }
+        return { value: "", done: true };
+      },
+    };
+  },
+};
+for (const val of iterableObj) {
+  console.log(val);
+}
+// This
+// is
+// iterable.
+//using generators
+function* iterableObj1() {
+  yield "This";
+  yield "is";
+  yield "iterable.";
+}
+for (const val of iterableObj1()) {
+  console.log(val);
+}
+console.log("=========================exampl4===========================");
+function* naturalNumbers() {
+  let num = 1;
+  while (true) {
+    yield num;
+    num = num + 1;
+  }
+}
+const numbers = naturalNumbers();
+console.log(numbers.next().value);
+console.log(numbers.next().value);
